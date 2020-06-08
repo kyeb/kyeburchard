6713e689f45ef99755b5f568d61b099a621b0f09
Kye Burchard <kyeb@mit.edu>
2020-06-07 21:01:42 -0600

    Fix IAP 2018 and format more docs

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 Personal website for me (Kye Burchard)
 
 Development:
+
 ```
 git clone git@github.com:kyeb/kyeburchard.git
 mv kyeburchard kyeb && cd kyeb
@@ -10,7 +11,8 @@ gulp dev
 ```
 
 The `dev` gulp task does a couple things:
+
 - Runs a BrowserSync server hosting the site locally
 - Watches for changes to `index.html` to reload
 - Watches for changes to `scss/styles.scss` to recompile to `css/styles.css`
-and injects the changes into the BrowserSync server, without reloading
+  and injects the changes into the BrowserSync server, without reloading
--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,47 @@
-var gulp = require('gulp'),
-  sass = require('gulp-sass'),
-  postcss = require('gulp-postcss'),
-  autoprefixer = require('autoprefixer'),
-  cssnano = require('cssnano')
-  browsersync = require('browser-sync').create()
-  rename = require('gulp-rename');
+var gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  postcss = require("gulp-postcss"),
+  autoprefixer = require("autoprefixer"),
+  cssnano = require("cssnano");
+browsersync = require("browser-sync").create();
+rename = require("gulp-rename");
 
 var paths = {
   styles: {
-    src: 'scss/styles.scss',
-    dest: 'css',
-    watch: 'scss/*'
-  }
-}
+    src: "scss/styles.scss",
+    dest: "css",
+    watch: "scss/*",
+  },
+};
 
-function style(){
-  return gulp.src(paths.styles.src)
-  .pipe(sass()).on('error', sass.logError)
-  .pipe(postcss([
-    autoprefixer(),
-    cssnano()
-  ]))
-  .pipe(rename("styles.min.css"))
-  .pipe(gulp.dest(paths.styles.dest))
-  .pipe(browsersync.stream())
+function style() {
+  return gulp
+    .src(paths.styles.src)
+    .pipe(sass())
+    .on("error", sass.logError)
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(rename("styles.min.css"))
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(browsersync.stream());
 }
 
 var reload = browsersync.reload;
 
-function dev(){
+function dev() {
   // compile scss initially
-  style()
+  style();
 
   // browsersync server
   browsersync.init({
     server: {
-      baseDir: "./"
-    }
-  })
+      baseDir: "./",
+    },
+  });
 
   // watch for style changes and compile
-  gulp.watch(paths.styles.watch, style)
+  gulp.watch(paths.styles.watch, style);
   gulp.watch("*.html").on("change", reload);
 }
 
-exports.style = style
-exports.dev = dev
+exports.style = style;
+exports.dev = dev;
